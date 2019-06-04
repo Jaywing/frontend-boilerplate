7bab59e86964093b2e49b60867072c3580cd4003
Joe Diton <joediton@gmail.com>
2019-06-04 12:58:39 +0100

    Added sample gulp tasks

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,6 +1,52 @@
-function defaultTask(cb) {
-  // place code for your default task here
+const { series, parallel } = require('gulp');
+
+function clean(cb) {
+  // body omitted
   cb();
 }
 
-exports.default = defaultTask;
+function cssTranspile(cb) {
+  // body omitted
+  cb();
+}
+
+function cssMinify(cb) {
+  // body omitted
+  cb();
+}
+
+function jsTranspile(cb) {
+  // body omitted
+  cb();
+}
+
+function jsBundle(cb) {
+  // body omitted
+  cb();
+}
+
+function jsMinify(cb) {
+  // body omitted
+  cb();
+}
+
+function livereload(cb) {
+  // body omitted
+  cb();
+}
+
+const watch = series(
+  clean,
+  parallel(cssTranspile, jsTranspile),
+  parallel(cssMinify, jsBundle),
+  jsMinify
+);
+
+const build = series(clean, parallel(cssTranspile, jsTranspile), jsBundle);
+
+// FIXME: start and build
+if (process.env.NODE_ENV === 'production') {
+  exports.default = build;
+} else {
+  exports.default = watch;
+}
