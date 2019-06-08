fbf8bb4ff559ddf1501199aa4389fb2bcaed716f
Joe Diton <joediton@gmail.com>
2019-06-08 21:25:18 +0100

    Removed xl breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ module.exports = {
     screens: {
       sm: '640px',
       md: '768px',
-      lg: '1024px',
-      xl: '1280px'
+      lg: '1024px'
     },
     colors: {
       transparent: 'transparent',
