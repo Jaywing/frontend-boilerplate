2a58850594695d811fcab7d1768839fc216228e3
Joe Diton <joediton@gmail.com>
2019-06-08 22:39:55 +0100

    Added PurgeCSS

--- gulpfile.js/tasks/cssTranspile.js
+++ gulpfile.js/tasks/cssTranspile.js
@@ -2,6 +2,7 @@ function cssTranspile() {
   const gulp = require('gulp');
   const paths = require('../../package.json').paths;
   const postcss = require('gulp-postcss');
+  const purgecss = require('gulp-purgecss');
   const rename = require('gulp-rename');
   const sourcemaps = require('gulp-sourcemaps');
 
@@ -23,12 +24,29 @@ function cssTranspile() {
       )
     )
     .pipe(
+      purgecss({
+        content: [paths.build + '**/*.html'],
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js']
+          }
+        ]
+      })
+    )
+    .pipe(
       rename(function(path) {
         path.extname = '.css';
       })
     )
     .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest(paths.css.dest));
+}
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
 }
 
 exports.cssTranspile = cssTranspile;
