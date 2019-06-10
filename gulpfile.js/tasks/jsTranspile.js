e5041d573c1128ab12d9a010c03b5cbfff8db2be
Joe Diton <joediton@gmail.com>
2019-06-10 15:45:12 +0100

    Added maps flag condtional to jsTranspile

--- gulpfile.js/tasks/jsTranspile.js
+++ gulpfile.js/tasks/jsTranspile.js
@@ -1,12 +1,14 @@
 function jsTranspile() {
+  const flags = require('../config/flags');
   const gulp = require('gulp');
   const paths = require('../../package.json').paths;
   const sourcemaps = require('gulp-sourcemaps');
+  const util = require('gulp-util');
 
   return gulp
     .src(paths.js.src + '*.js')
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('maps'))
+    .pipe(flags.maps ? sourcemaps.init() : util.noop())
+    .pipe(flags.maps ? sourcemaps.write('maps') : util.noop())
     .pipe(gulp.dest(paths.js.dest));
 }
 
