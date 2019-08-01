ee629df2443cebe508194c61ab1cd9f05837bee2
Joe Diton <joediton@gmail.com>
2019-08-01 14:33:53 +0100

    Implemented js module calls

--- _src/js/index.js
+++ _src/js/index.js
@@ -1,1 +1,21 @@
-console.log("JS Loaded");
+const jsElements = document.querySelectorAll("[data-js]");
+
+for (let i = 0; i < jsElements.length; i++) {
+  const el = jsElements[i];
+
+  let jsElementFunctions = el.getAttribute("data-js");
+  jsElementFunctions = jsElementFunctions.split(" ").map(function(item) {
+    return item.trim(jsElementFunctions);
+  });
+
+  let options = {};
+  const dataOptions = el.getAttribute(`data-js-options`);
+
+  if (dataOptions) {
+    options = JSON.parse(dataOptions);
+  }
+
+  for (let i = 0; i < jsElementFunctions.length; i++) {
+    window[jsElementFunctions[i]](el, options[i]);
+  }
+}
