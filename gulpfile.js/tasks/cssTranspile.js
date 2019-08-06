a36f6dd1f6a7437fcfdf9967038edc3f6bd015a8
Joe Diton <joediton@gmail.com>
2019-08-06 15:34:54 +0100

    Added gulp-sass

--- gulpfile.js/tasks/cssTranspile.js
+++ gulpfile.js/tasks/cssTranspile.js
@@ -3,30 +3,32 @@ function cssTranspile() {
   const flags = require("../config/flags");
   const gulp = require("gulp");
   const paths = require("../../package.json").paths;
-  const postcss = require("gulp-postcss");
+  // const postcss = require("gulp-postcss");
   const purgecss = require("gulp-purgecss");
   const rename = require("gulp-rename");
+  const sass = require("gulp-sass");
   const sourcemaps = require("gulp-sourcemaps");
-  const tailwindcss = require("tailwindcss");
+  // const tailwindcss = require("tailwindcss");
   const util = require("gulp-util");
 
   return (
     gulp
       .src(paths.css.src + "!(_)*.scss")
       .pipe(flags.maps ? sourcemaps.init() : util.noop())
-      .pipe(
-        postcss(
-          [
-            require("postcss-import"),
-            require("precss"),
-            tailwindcss("./gulpfile.js/config/tailwind.js"),
-            require("autoprefixer")
-          ],
-          {
-            syntax: require("postcss-scss")
-          }
-        )
-      )
+      .pipe(sass().on("error", sass.logError))
+      // .pipe(
+      //   postcss(
+      //     [
+      //       require("postcss-import"),
+      //       require("precss"),
+      //       tailwindcss("./gulpfile.js/config/tailwind.js"),
+      //       require("autoprefixer")
+      //     ],
+      //     {
+      //       syntax: require("postcss-scss")
+      //     }
+      //   )
+      // )
       .pipe(flags.minify ? cssnano() : util.noop())
       // FIXME: PurgeCSS error: Unexpected '/'. Escaping special characters with \ may help.
       .pipe(
