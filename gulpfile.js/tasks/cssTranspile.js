0df2f30c92b58224d4cfbf444d80282abdfdee4f
Joe Diton <joe.diton@jaywing.com>
2019-09-06 20:20:49 +0100

    Fixed PurgeCSS

--- gulpfile.js/tasks/cssTranspile.js
+++ gulpfile.js/tasks/cssTranspile.js
@@ -8,54 +8,27 @@ function cssTranspile() {
   const rename = require("gulp-rename");
   const sass = require("gulp-sass");
   const sourcemaps = require("gulp-sourcemaps");
-  // const tailwindcss = require("tailwindcss");
   const util = require("gulp-util");
 
-  return (
-    gulp
-      .src(paths.css.src + "!(_)*.scss")
-      .pipe(flags.maps ? sourcemaps.init() : util.noop())
-      .pipe(sass().on("error", sass.logError))
-      // .pipe(
-      //   postcss(
-      //     [
-      //       require("postcss-import"),
-      //       require("precss"),
-      //       tailwindcss("./gulpfile.js/config/tailwind.js"),
-      //       require("autoprefixer")
-      //     ],
-      //     {
-      //       syntax: require("postcss-scss")
-      //     }
-      //   )
-      // )
-      .pipe(flags.minify ? cssnano() : util.noop())
-      // FIXME: PurgeCSS error: Unexpected '/'. Escaping special characters with \ may help.
-      .pipe(
-        flags.purge
-          ? purgecss({
-              content: [paths.build + "**/*.html", paths.build + "**/*.js"],
-              extractors: [
-                {
-                  extractor: class TailwindExtractor {
-                    static extract(content) {
-                      return content.match(/[A-Za-z0-9-_:/]+/g) || [];
-                    }
-                  },
-                  extensions: ["html", "js"]
-                }
-              ]
-            })
-          : util.noop()
-      )
-      .pipe(
-        rename(function(path) {
-          path.extname = ".css";
-        })
-      )
-      .pipe(flags.maps ? sourcemaps.write("maps") : util.noop())
-      .pipe(gulp.dest(paths.css.dest))
-  );
+  return gulp
+    .src(paths.css.src + "!(_)*.scss")
+    .pipe(flags.maps ? sourcemaps.init() : util.noop())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(flags.minify ? cssnano() : util.noop())
+    .pipe(
+      flags.purge
+        ? purgecss({
+            content: [paths.build + "**/*.html", paths.build + "**/*.js"]
+          })
+        : util.noop()
+    )
+    .pipe(
+      rename(function(path) {
+        path.extname = ".css";
+      })
+    )
+    .pipe(flags.maps ? sourcemaps.write("maps") : util.noop())
+    .pipe(gulp.dest(paths.css.dest));
 }
 
 exports.cssTranspile = cssTranspile;
