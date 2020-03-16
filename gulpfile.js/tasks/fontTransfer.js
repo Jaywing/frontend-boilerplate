bd2879be0d2914ef3b9041eb0387c76f898934d5
Joe Diton <joediton@gmail.com>
2020-03-16 14:13:56 +0000

    Created fontTransfer gulp task file

---
+++ gulpfile.js/tasks/fontTransfer.js
@@ -0,0 +1,13 @@
+function fontTransfer() {
+  const flags = require('../config/flags');
+  const gulp = require('gulp');
+  const paths = require('../../package.json').paths;
+
+  return gulp
+    .src(paths.fonts.src)
+    .pipe(
+      gulp.dest(flags.static ? paths.fonts.static_dest : paths.fonts.proxy_dest)
+    );
+}
+
+exports.fontTransfer = fontTransfer;
