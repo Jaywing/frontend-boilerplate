bcb5cd20069162e5899ac8eb4e8d7a98ade4f446
Joe Diton <joediton@gmail.com>
2020-03-17 19:23:44 +0000

    Added data-js modules to main.ts

--- _src/ts/main.ts
+++ _src/ts/main.ts
@@ -1,3 +1,27 @@
-import writeText from "./modules/writeText";
+// data-js modules
+import dataJsModule from './modules/extends/dataJsModule';
 
-writeText("Hello world");
+const dataJsElements = document.querySelectorAll('[data-js]');
+
+for (let i = 0; i < dataJsElements.length; i++) {
+  const el = dataJsElements[i];
+
+  const dataJsValue = el.getAttribute('data-js');
+  const dataJsModules: any = dataJsValue.split(' ').map(function(item) {
+    return item.trim();
+  });
+
+  let options = [{}];
+  const dataOptions = el.getAttribute(`data-js-options`);
+
+  if (dataOptions) {
+    options = JSON.parse(dataOptions);
+  }
+
+  for (let name = 0; name < dataJsModules.length; name++) {
+    import(`./modules/${dataJsModules[name]}`).then(Module => {
+      const module: dataJsModule = new Module.default(el, options[name]);
+      module.init();
+    });
+  }
+}
