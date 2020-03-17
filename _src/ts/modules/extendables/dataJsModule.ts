2b7287716cb8dc82276ea150383a6e66aa28c46d
Joe Diton <joediton@gmail.com>
2020-03-17 19:26:57 +0000

    Created dataJsModule extendable

---
+++ _src/ts/modules/extendables/dataJsModule.ts
@@ -0,0 +1,11 @@
+export default class dataJsModule {
+  el: HTMLElement;
+  options: object;
+
+  constructor(el: HTMLElement, options: object) {
+    this.el = el;
+    this.options = options;
+  }
+
+  init() {}
+}
