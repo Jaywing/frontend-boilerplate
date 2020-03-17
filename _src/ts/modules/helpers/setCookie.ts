a794de84494b70ca7349fb6146fd28dd56cac0a8
Joe Diton <joediton@gmail.com>
2020-03-17 19:27:35 +0000

    Created setCookie helper

---
+++ _src/ts/modules/helpers/setCookie.ts
@@ -0,0 +1,11 @@
+export default function setCookie(name: string, value: string, days: number) {
+  var expires = '';
+
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
