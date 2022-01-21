e2d92237874025a4a9fdb71afc276bf6b6dc2ad2
Joe Diton <joe.diton@jaywing.com>
2022-01-21 11:59:32 +0000

    Added postcss and Tailwind to cssTranspile

--- gulpfile.js/tasks/cssTranspile.js
+++ gulpfile.js/tasks/cssTranspile.js
@@ -1,17 +1,26 @@
 function cssTranspile() {
   const autoprefixer = require("autoprefixer");
   const cssnano = require("cssnano");
-  const flags = require("../config/flags");
+  const flags = require("../../cli-flags.config");
   const gulp = require("gulp");
   const gulpif = require("gulp-if");
   const paths = require("../../package.json").paths;
   const postcss = require("gulp-postcss");
+  const postcssnested = require("postcss-nested");
+  const postcsscombinemediaquery = require("postcss-combine-media-query");
+  const postcsseasyimport = require("postcss-easy-import");
   const purgecss = require("gulp-purgecss");
   const purgewhitelist = require("../../src/scss/whitelist.json");
-  const sass = require("gulp-sass");
+  const sass = require("gulp-sass")(require("sass"));
   const sourcemaps = require("gulp-sourcemaps");
+  const tailwindcss = require("tailwindcss");
 
-  const postcssplugins = [autoprefixer()];
+  const postcssplugins = [
+    postcsseasyimport(),
+    postcssnested(),
+    tailwindcss(),
+    autoprefixer(),
+  ];
 
   if (flags.minify) {
     postcssplugins.push(
@@ -21,9 +30,13 @@ function cssTranspile() {
     );
   }
 
+  if (flags.combinemq) {
+    postcssplugins.push(postcsscombinemediaquery());
+  }
+
   return gulp
     .src(paths.css.src + "!(_)*.scss")
-    .pipe(gulpif(flags.maps, sourcemaps.init()))
+    .pipe(gulpif(flags.sourcemaps, sourcemaps.init()))
     .pipe(sass().on("error", sass.logError))
     .pipe(postcss(postcssplugins))
     .pipe(
@@ -40,7 +53,7 @@ function cssTranspile() {
         })
       )
     )
-    .pipe(gulpif(flags.maps, sourcemaps.write("maps")))
+    .pipe(gulpif(flags.sourcemaps, sourcemaps.write("maps")))
     .pipe(gulp.dest(flags.proxy ? paths.css.proxyDest : paths.css.staticDest));
 }
 
